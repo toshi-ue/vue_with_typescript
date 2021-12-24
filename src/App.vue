15f68495abcb784b31c876753d86bdd75df15c5b
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-12-24 13:26:29 +0900

    update: typescriptのinterfaceを使用してjobを定義、表示する

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,46 @@
 <template>
   <div class="app">
-    <p>{{ name }}: {{ age }}</p>
-    <button @click="changeName('45')">change name</button>
-    <button @click="changeAge('45')">change age</button>
+    <p>{{ jobs[0].location }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from "vue";
+import Job from "./types/job";
 
 export default defineComponent({
   name: "App",
   components: {},
   setup() {
-    // const state = reactive({
-    //   name: "Link",
-    //   age: 25 as number | string,
-    // });
-    // state.name = ref("Link"); // cannot change type
-    // state.age = "26";
-    // return { ...toRefs(state) };
-    // const name = ref("Link");
-    // const age = ref(25) as number | string; // cannot write like this
-    // const age = ref<number | string>(25);
-    // return { name, age };
-  },
-  // data() {
-  //   return {
-  //     name: "hello, Vue With Typescript",
-  //     age: 25 as number | string,
-  //   };
-  // },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-    },
-    changeAge(age: string | number) {
-      this.age = age;
-      return age;
-    },
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    return { jobs };
   },
 });
 </script>
