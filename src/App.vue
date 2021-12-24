aef70d81cdede302f3a92d9931006045adf8aa23
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-12-24 11:04:09 +0900

    update: reactive, refを使用した書き方

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,30 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, ref, toRefs } from "vue";
 
 export default defineComponent({
   name: "App",
   components: {},
-  data() {
-    return {
-      name: "hello, Vue With Typescript",
-      age: 25 as number | string,
-    };
+  setup() {
+    // const state = reactive({
+    //   name: "Link",
+    //   age: 25 as number | string,
+    // });
+    // state.name = ref("Link"); // cannot change type
+    // state.age = "26";
+    // return { ...toRefs(state) };
+    // const name = ref("Link");
+    // const age = ref(25) as number | string; // cannot write like this
+    // const age = ref<number | string>(25);
+    // return { name, age };
   },
+  // data() {
+  //   return {
+  //     name: "hello, Vue With Typescript",
+  //     age: 25 as number | string,
+  //   };
+  // },
   methods: {
     changeName(name: string) {
       this.name = name;
