a1d26c84894fcceeb40392c919d7c8f8b673dd17
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-12-29 23:17:28 +0900

    update: fontawesomeを使用するための設定

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "./assets/global.css";
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faExternalLinkAlt, faGithub)
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
