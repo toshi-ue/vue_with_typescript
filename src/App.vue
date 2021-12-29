088ef21fb2822cf51aad59485268c9d88ce6a507
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-12-29 21:52:06 +0900

    update: types と props を使用して定義、データの一覧表示

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from "vue";
 import Job from "./types/job";
+import JobList from "./components/JobList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
       {
