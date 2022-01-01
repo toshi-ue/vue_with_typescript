00f4fe71e97a475597f630a0818da805ffd706b0
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-01-01 18:01:27 +0900

    update: computed関数を使用して並べ替え機能の追加

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -2,7 +2,7 @@
   <div class="job-list">
     <p>Orderd by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderdJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} rupees</p>
@@ -21,7 +21,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import Job from "@/types/job";
 import OrderTerm from "@/types/OrderTerm";
 
@@ -36,6 +36,15 @@ export default defineComponent({
       required: true,
       type: String as PropType<OrderTerm>,
     },
+  },
+  setup(props) {
+    const orderdJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+
+    return { orderdJobs };
   },
 });
 </script>
